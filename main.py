77caecd87cf212c8026562da798310c264c68f4c
Rtiscev <98522787+Rtiscev@users.noreply.github.com>
2024-07-09 15:47:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# palindrome problem:
+# racecar
+# level
+# noon
+# rotor 
+# they are all palindrome
+
+inpStr=input()
+isPalindrome=False
+# add code here that will check whether input string is
+# Palindrome or not
+
+
+
+print(isPalindrome)
